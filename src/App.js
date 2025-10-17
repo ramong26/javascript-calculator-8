07cfad94eab63cbadb3af02e5ce7c9340e481018
김수연 <rlatndus21049@gmail.com>
2025-10-17 13:31:29 +0900

    feat(calculator): implement custom delimiter processing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요. \n");
+
+    // 빈 문자열일 경우 
+    if(!input){
+      Console.print("결과 : 0");
+      return;
+    }
+
+    let numbers;
+
+    if(input.startsWith("//")) {
+      const customDelimiterMatch = input.match(/^\/\/(.)\n(.*)$/);
+      if(customDelimiterMatch){
+        const delimiter = customDelimiterMatch[1];
+        const numberString = customDelimiterMatch[2];
+        numbers = numberString.split(delimiter);
+      } else {
+        numbers = input.split(/[,:]/)
+      }
+    } else {
+      numbers = input.split(/[,:]/)
+    }
+
+    let sum = 0;
+
+    for(const num of numbers){
+      const number = Number(num);
+      
+      // 음수 검증
+      if (number < 0) {
+        throw new Error("[ERROR]");
+      }
+      
+      sum += number || 0;
+    }
+
+    Console.print(`결과: ${sum}`)
+  }
 }
 
 export default App;
