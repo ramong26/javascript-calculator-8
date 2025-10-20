7005ac17cbad83beb54e2dd2f30aee06f0113c40
김수연 <rlatndus21049@gmail.com>
2025-10-20 22:04:07 +0900

    refactore: 에러 핸들링 입력값 없을 때 추가

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요. \n");
+  async run () {
+    const numberInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
 
-    // 빈 문자열일 경우 
-    if(!input){
-      Console.print("결과 : 0");
-      return;
-    }
+    // 입력하지 않을 경우 결과 0 출력
+    if(!numberInput || numberInput.length === 0){
+      throw new Error('[ERROR] 덧셈할 문자열을 입력해 주세요.')
+    } 
 
     let numbers;
 
-    if(input.startsWith("//")) {
-      const customDelimiterMatch = input.match(/^\/\/(.)\n(.*)$/);
-      if(customDelimiterMatch){
+    if(numberInput.startsWith('//')) {
+      // \\n을 \n으로 변화
+      const changeSlash = numberInput.replace(/\\n/g, '\n');
+      const customDelimiterMatch = changeSlash.match(/^\/\/(.)\n(.*)$/); //  ["//;\n1",";","1"]
+   
+      if(customDelimiterMatch) {
         const delimiter = customDelimiterMatch[1];
         const numberString = customDelimiterMatch[2];
-        numbers = numberString.split(delimiter);
+        numbers = numberString.split(delimiter) // 분리된 숫자들
       } else {
-        numbers = input.split(/[,:]/)
+        numbers = numberInput.split(/[,:]/) 
       }
     } else {
-      numbers = input.split(/[,:]/)
+      numbers = numberInput.split(/[,:]/) 
     }
 
-    let sum = 0;
+    let sumNumbers = 0;
 
-    for(const num of numbers){
-      const number = Number(num);
-      
-      // 음수 검증
-      if (number < 0) {
-        throw new Error("[ERROR]");
+    for(const num of numbers) {
+      const parsedNumber = Number(num)
+
+      if(parsedNumber < 0) {
+        throw new Error('[ERROR] 음수는 입력할 수 없습니다. ')
       }
-      
-      sum += number || 0;
+
+      sumNumbers += parsedNumber || 0
     }
 
-    Console.print(`결과: ${sum}`)
+    Console.print(`결과 : ${sumNumbers}`)
   }
 }
-
 export default App;
+
+// 음수 검증은 초반에 하는 것이 아닌 커스텀과 문자열 분리 후 하는 것이 옳다. -> 숫자끼리 비교가 되어야 하기 때문
